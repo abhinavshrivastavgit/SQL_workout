f523fb867bc8e4700c29a10a7b2eb8c0008da233
Abhinav Shrivastav <abhinavshrivastavgithub@gmail.com>
2026-01-15 02:32:59 +0530

    jan15_basic_select

---
+++ daily_script_drill/Jan15-Basics.sql
@@ -0,0 +1,17 @@
+--retrieve all customers data
+USE SalesDB
+
+
+SELECT *
+FROM Sales.customers
+
+
+USE AdventureWorksDW2025
+
+SELECT *
+-- FROM dbo.Databaselog
+FROM dbo.AdventureWorksDWBuildVersion
+
+
+
+
