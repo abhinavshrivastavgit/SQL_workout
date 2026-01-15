18f6e49cda7a5c6521e7e1c6679c8e0dc61844a2
Abhinav Shrivastav <abhinavshrivastavgithub@gmail.com>
2026-01-15 14:40:34 +0530

    sorted the table and print output only for score above 450

--- daily_script_drill/Jan15-Basics_02.sql
+++ daily_script_drill/Jan15-Basics_02.sql
@@ -1,9 +1,19 @@
 --Retrieve each customer's country name, country and score
 --in this file we will deepen our roots into the concept.
 
-USE MyDatabase
-SELECT 
-    first_name,
+USE MyDatabase       --MyDatabase is the database we are using
+SELECT               --here we are more specific what we are choosing/selecting from the table
+    first_name, 
     country,
     score
+FROM customers       -- this the table from database
+ 
+
+-- now we sorted the table 
+SELECT
+   first_name,
+   country,
+   score
 FROM customers
+WHERE score>450  -- according to the scores which is above 450
+
