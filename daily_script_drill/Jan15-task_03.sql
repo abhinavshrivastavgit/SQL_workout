b21edd264d3563552bcc77b49bb15af6cdd479b4
Abhinav Shrivastav <abhinavshrivastavgithub@gmail.com>
2026-01-15 17:30:02 +0530

    Retrieve all customers and sort the results by the country and then the highest score

---
+++ daily_script_drill/Jan15-task_03.sql
@@ -0,0 +1,9 @@
+-- Retrieve all customers and sort the results by the country and then the highest score
+
+USE MyDatabase
+--sorting based on multiple factor
+SELECT *
+FROM customers       --from table customers
+ORDER BY 
+country ASC,         --first based on country
+score DESC           --then arrange it within based on score
