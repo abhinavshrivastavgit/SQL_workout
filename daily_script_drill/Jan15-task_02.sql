0d8139e340f63f8b09824917ec285c162091eb2f
Abhinav Shrivastav <abhinavshrivastavgithub@gmail.com>
2026-01-15 17:22:35 +0530

    Retrieve all customers and sort the result based on country first then there score

--- daily_script_drill/Jan15-task_02.sql
+++ daily_script_drill/Jan15-task_02.sql
@@ -3,4 +3,11 @@
 USE MyDatabase
 SELECT *
 FROM customers       --from table customers
-ORDER BY score ASC   --to arrange lowest first as per the score 
+ORDER BY score ASC   --to arrange lowest first as per the score
+
+--sorting based on multiple factor
+SELECT *
+FROM customers       --from table customers
+ORDER BY 
+country ASC,         --first based on country
+score DESC           --then arrange it within based on score
