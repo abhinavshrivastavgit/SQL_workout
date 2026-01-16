dcf9d4b2afb25806c380c7cc36ae323f3e0c9138
Abhinav Shrivastav <abhinavshrivastavgithub@gmail.com>
2026-01-17 04:19:23 +0530

    feat: Notes01 SQL BASIC (USE > SELECT > FROM)

---
+++ notes/sql_notes01.sql
@@ -0,0 +1,14 @@
+-- NOTES 01
+-- here we will understand how SELECT,FROM AND USE WORKS
+
+
+USE MyDatabase       --MyDatabase is the database we are using
+SELECT *             -- Here we are selecting all columns           
+FROM customers 
+
+
+SELECT             --here we are more specific what we are choosing/selecting from the table
+id,
+first_name,
+score                    
+FROM customers    --the table we are using from the database
