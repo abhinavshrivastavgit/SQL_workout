edc572397e1df840d9cc8d53e9d25fa769c8deec
Abhinav Shrivastav <abhinavshrivastavgithub@gmail.com>
2026-01-17 00:14:59 +0530

    find the average score for each country considering only customers with a score not equal to 0 and return only those countries with an average score greater than 430.

--- daily_script_drill/Jan16-task_01.sql
+++ daily_script_drill/Jan16-task_01.sql
@@ -1,8 +1,17 @@
 -- find the average score for each country considering only customers with a score not equal to 0 and return only those countries with an average score greater than 430.
 
 USE MyDatabase
+
 SELECT
-id, 
 country,
-score
+AVG(score) AS avg_score
 FROM customers
+GROUP BY country
+
+SELECT
+country,
+AVG(score) AS avg_score
+FROM customers
+WHERE score != 0
+GROUP BY country
+HAVING AVG(score) > 450
