cb55eb57056fef42dddae30fa6e551cd6d338fcc
Abhinav Shrivastav <abhinavshrivastavgithub@gmail.com>
2026-01-19 02:21:07 +0530

    add: use of where filter

--- notes/sql_notes02.sql
+++ notes/sql_notes02.sql
@@ -1,4 +1,15 @@
 USE AI_Product_DB
 
-SELECT *
-FROM app_users
+--selecting all columns from the table
+SELECT * FROM app_users     -- to compare changes...
+
+-- -- use of WHERE 
+-- -- TARGETING ACTIVE USERS
+-- SELECT user_id, country, price_paid
+-- FROM app_users
+-- WHERE last_active > '2026-01-01';
+
+-- use of AND with WHERE 
+SELECT user_id, country, subscription_plan 
+FROM app_users 
+WHERE country = 'India' AND subscription_plan = 'Pro';
