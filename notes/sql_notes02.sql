4435ba9fad6131eb6b2f07533f1a9a636e4c2f7d
Abhinav Shrivastav <abhinavshrivastavgithub@gmail.com>
2026-01-19 02:21:56 +0530

    add: use of AND with WHERE

--- notes/sql_notes02.sql
+++ notes/sql_notes02.sql
@@ -3,11 +3,16 @@ USE AI_Product_DB
 --selecting all columns from the table
 SELECT * FROM app_users     -- to compare changes...
 
+-- ======================================================
+-- use ctrl+/ to use particular code block
+-- ======================================================
+
 -- -- use of WHERE 
 -- -- TARGETING ACTIVE USERS
 -- SELECT user_id, country, price_paid
 -- FROM app_users
 -- WHERE last_active > '2026-01-01';
+
 
 -- use of AND with WHERE 
 SELECT user_id, country, subscription_plan 
