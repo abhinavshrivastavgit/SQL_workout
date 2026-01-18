71d0f9387ffde78cc69696bae02c383bda493bc3
Abhinav Shrivastav <abhinavshrivastavgithub@gmail.com>
2026-01-19 00:47:21 +0530

    Task: use of ALTER

---
+++ daily_script_drill/Jan18-task02.sql
@@ -0,0 +1,5 @@
+-- add a new column called email to the persons table
+
+USE MyDatabase
+ALTER TABLE persons                  -- alter means we are making some change in the table.
+ADD email VARCHAR(50) NOT NULL
