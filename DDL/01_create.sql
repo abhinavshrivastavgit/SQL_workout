66baa48b52cf931721484d0f2f5e3bc1ceaaca43
Abhinav Shrivastav <abhinavshrivastavgithub@gmail.com>
2026-01-20 01:31:29 +0530

    add: 01_create.sql-> table created

---
+++ DDL/01_create.sql
@@ -0,0 +1,10 @@
+-- Create a new table called details with columns: id, person_name,birth_date , and phone
+
+USE MyDatabase
+CREATE TABLE details (
+    id INT NOT NULL,                         -- here "id" is the name of the column "INT" is the data type and "NOT NULL" id the constraint.
+    person_name VARCHAR(50) NOT NULL,        -- here "person_name" is the name of the column "VARCHAR" is the data type, "(50)" is the limit of the variable and "NOT NULL" id the constraint.here "not null" says it cant be empty
+    birth_date DATE,
+    phone VARCHAR NOT NULL
+    CONSTRAINT pk_persons PRIMARY KEY (id)
+)
---
+++ DDL/02_check_table.sql
@@ -0,0 +1,5 @@
+-- now checking our table that we had created
+
+USE MyDatabase
+SELECT * 
+FROM details 
