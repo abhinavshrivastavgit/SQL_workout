7399bc8a8c38d8cf73cd0ad3bdebb20ff916cbae
Abhinav Shrivastav <abhinavshrivastavgithub@gmail.com>
2026-01-20 22:15:13 +0530

    add: created table details

--- DML/01_createTable.sql
+++ DML/01_createTable.sql
@@ -5,7 +5,7 @@ USE MyDatabase
 -- creatinfg table "deatails" contains information like id, first_name, country and score.
 CREATE Table details (
   id INT NOT NULL  ,
-  country CHAR(500) NOT NULL,
+  country VARCHAR(500) NOT NULL,
   full_name VARCHAR(100) NOT NULL,
   designation VARCHAR(100) ,
   score INT,
