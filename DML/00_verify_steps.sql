bad75e0ad8a0c8727ec3b7a12dec443b5a818a8f
Abhinav Shrivastav <abhinavshrivastavgithub@gmail.com>
2026-01-20 21:18:30 +0530

    add: verify command file to check the changes

--- DML/00_verify_steps.sql
+++ DML/00_verify_steps.sql
@@ -1,9 +1,13 @@
 -- Using Database "My_Database"
 USE MyDatabase
 
+/*
+remove comment to run either of the programs
+*/
+
 -- using this command to verify the changes we made in the coming files
-SELECT *
-FROM details
+-- SELECT *
+-- FROM details
 
 
 -- USING THIS COMMAND TO DELETE THE TABLE
