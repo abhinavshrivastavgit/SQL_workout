87ccda0adfac755c08b9e9ef20e6c512fd2e028b
Abhinav Shrivastav <abhinavshrivastavgithub@gmail.com>
2026-01-24 22:07:26 +0530

    task: logical operator 'OR'

---
+++ filtering_data/task07.sql
@@ -0,0 +1,11 @@
+-- logical operator
+-- AND operator --> All condition must be true.
+
+-- country must be "USA" and score must be > 500
+
+USE MyDatabase
+
+SELECT *
+FROM customers 
+WHERE country = 'USA' AND score > 500
+
